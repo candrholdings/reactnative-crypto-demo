b046deecac76b51078ad744faf7173b13eb8e8cf
William Wong <compulim@hotmail.com>
2016-05-26 11:04:40 +0800

    Update bridging header to vanilla format

--- ios/EncryptNatively-Bridging-Header.h
+++ ios/EncryptNatively-Bridging-Header.h
@@ -1,9 +1,5 @@
 //
-//  EncryptNatively-Bridging-Header.h
-//  EncryptNatively
-//
-//  Created by William Wong on 25/5/2016.
-//  Copyright © 2016 Facebook. All rights reserved.
+//  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
 #import "RCTBridgeModule.h"
