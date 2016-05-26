74b7dc22437c2d331c15237b1b2b4a05f7f39e81
William Wong <compulim@hotmail.com>
2016-05-27 03:11:49 +0800

    Initial commit

---
+++ ios/CryptoProvider.m
@@ -0,0 +1,13 @@
+//
+//  CryptoProvider.m
+//  EncryptNatively
+//
+//  Created by William Wong on 27/5/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(CryptoProvider, NSObject)
+@end
---
+++ ios/CryptoProvider.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoProvider.swift
+//  EncryptNatively
+//
+//  Created by William Wong on 27/5/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+import Foundation
+
+@objc(CryptoProvider)
+class CryptoProvider: NSObject {
+}
